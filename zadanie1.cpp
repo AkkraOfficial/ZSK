fc5e97f948616897409c00ced9b0beae145d0182
AKopydlowski <90185121+AKopydlowski@users.noreply.github.com>
2021-09-14 15:43:41 +0200

    Add files via upload

---
+++ zadanie1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangle{
+	public:
+	float dlugosca;
+	float dlugoscb;
+	float pole;
+	float obwod;
+	
+	void pole();
+	void obwod();
+};
+
+void Rectangle::pole(){
+	cout<<"podaj d³ugoœæ a: ";
+	cin>>dlugosca;
+	cout<<"podaj d³ugoœæ b: ";
+	cin>>dlugoscb;  
+	float pole =
+}
+
+int main(int argc, char** argv) {
+	setlocale(LC_CTYPE, "polish");
+
+	return 0;
+}
